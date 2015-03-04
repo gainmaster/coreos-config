2ad9b049d9dca2016ef116c52753c89fa141305b
Tony Hesjevik <tony@hesjevik.no>
2015-03-04 20:21:28 +0100

    Run registrator in foreground

--- coreos/update_cloud_config.sh
+++ coreos/update_cloud_config.sh
@@ -4,14 +4,17 @@
 #
 #
 
-echo "[update_cloud_config.sh] Updating CoreOS cloud config for ${HOSTNAME}."
+echo "[update_cloud_config.sh] - Updating CoreOS cloud config for ${HOSTNAME}."
 
 # Delete old config
 sudo rm -f /var/lib/coreos-install/user_data
+echo "[update_cloud_config.sh] - Old config deleted"
 
 # Insert new config
 sudo cp ./${HOSTNAME}-config.yml /var/lib/coreos-install/user_data
+echo "[update_cloud_config.sh] - New config inserted"
 
+echo ""
 echo "==============================================================================="
 echo "[update_cloud_config.sh] - Update of CoreOS cloud config completed."
 echo "|update_cloud_config.sh] - Run 'locksmithctl reboot' to load new configuration."

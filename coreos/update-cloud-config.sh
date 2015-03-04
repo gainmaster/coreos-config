9c6c9d07befd4ae0ba0c94e77938e1cd91e0e759
Tony Hesjevik <tony@hesjevik.no>
2015-03-04 15:15:12 +0100

    Make update executable

--- coreos/update-cloud-config.sh
+++ coreos/update-cloud-config.sh
@@ -4,15 +4,15 @@
 #
 #
 
-echo 'Updating CoreOS cloud config for ${HOSTNAME}.'
+echo "Updating CoreOS cloud config for ${HOSTNAME}."
 
 # Delete old config
 sudo rm -f /var/lib/coreos-install/user_data
-echo 'Deleted old config at /var/lib/coreos-install/user_data.'
+echo "Deleted old config at /var/lib/coreos-install/user_data."
 
 # Insert new config
 sudo cp ./${HOSTNAME}-config.yml /var/lib/coreos-install/user_data
-echo 'Copied new config ${HOSTNAME}-config.yml to /var/lib/coreos-install/user_data.'
+echo "Copied new config ${HOSTNAME}-config.yml to /var/lib/coreos-install/user_data."
 
-echo 'Update of CoreOS cloud config completed.'
-echo 'Run "locksmithctl reboot" to load new configuration.'
+echo "Update of CoreOS cloud config completed."
+echo "Run "locksmithctl reboot" to load new configuration."

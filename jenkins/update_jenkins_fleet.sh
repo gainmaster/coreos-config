bf85919e11bcaa23e10a84b1f625e4665413648b
Tony Hesjevik <tony@hesjevik.no>
2015-03-11 12:20:51 +0100

    Update jenkins script

--- jenkins/update_jenkins_fleet.sh
+++ jenkins/update_jenkins_fleet.sh
@@ -11,15 +11,12 @@ echo ""
 
 # Stop old jenkins units
 fleetctl list-units --no-legend=true | grep jenkins | awk 'NR =1 {print $1}' | xargs -l fleetctl stop
-echo "[update_jenkins_fleet.sh] - Old jenkins fleetd units stoped"
 
 # Destroy old jenkins units
 fleetctl list-unit-files --no-legend=true | grep jenkins | awk 'NR =1 {print $1}' | xargs -l fleetctl destroy
-echo "[update_jenkins_fleet.sh] - Old jenkins fleetd units destroyed"
 
 # Insert new jenkins units
 fleetctl submit jenkins-master@.service jenkins-master-registrator@.service
-echo "[update_jenkins_fleet.sh] - New jenkins fleetd units submited"
 
 echo ""
 echo "==============================================================================="

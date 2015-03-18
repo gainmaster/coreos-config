c6f9d2edfc36135722d55664b282a179294d16c7
Knut Lorvik <lorviken@gmail.com>
2015-03-18 14:35:03 +0100

    Rename gainmaster-frontend to gainmaster-webclient

--- gainmaster-webclient/start_gainmaster_webclient.sh
+++ gainmaster-webclient/start_gainmaster_webclient.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
 # 
-# Start Gainmaster frontend fleetd units
+# Start gainmaster-webclient fleetd units
 #
 
 echo "==============================================================================================="
-echo "[start_frontend.sh] - Starting Gainmaster frontend fleetd units."
+echo "[start_frontend.sh] - Starting gainmaster-webclient fleetd units."
 echo "==============================================================================================="
 echo ""
 
 # Start Jenkins master fleetd unit
-fleetctl start gainmaster-frontend-angular@${1}.service gainmaster-frontend-nginx@${1}.service gainmaster-frontend-registrator@${1}.service
+fleetctl start gainmaster-webclient-angular@${1}.service gainmaster-webclient-nginx@${1}.service gainmaster-webclient-registrator@${1}.service
 
 echo ""
 echo "==============================================================================================="
-echo "[start_frontend.sh] - Gainmaster frontend fleetd units started."
+echo "[start_frontend.sh] - gainmaster-webclient fleetd units started."
 echo "==============================================================================================="

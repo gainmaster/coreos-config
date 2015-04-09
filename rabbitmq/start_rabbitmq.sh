bd24d068672c69f0dbb1c8bdc20589ba747d7360
Knut Lorvik <lorviken@gmail.com>
2015-04-09 15:41:28 +0200

    Add rabbitmq service files

---
+++ rabbitmq/start_rabbitmq.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# 
+# Start RabbitMQ fleetd units
+#
+
+echo "==============================================================================================="
+echo "[start_rabbitmq.sh] - Starting RabbitMQ fleetd units."
+echo "==============================================================================================="
+echo ""
+
+# Start Vulcand fleetd unit
+fleetctl start rabbitmq@${1}.service rabbitmq-registrator@${1}.service
+
+echo ""
+echo "==============================================================================================="
+echo "[start_rabbitmq.sh] - RabbitMQ fleetd units started."
+echo "==============================================================================================="
